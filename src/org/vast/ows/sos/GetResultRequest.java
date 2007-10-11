aac97f6ecc261b386b6bd9991db2486c9ba1bc14
tonycook <tcook@nsstc.uah.edu>
2007-10-11 20:21:48 +0000

    Stub for implementing getResult operation
    git-svn-id: http://ows-service-framework.googlecode.com/svn/common@139 65eab6d0-0751-0410-986a-37c5601cf520

---
+++ src/org/vast/ows/sos/GetResultRequest.java
@@ -0,0 +1,49 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1.1.html
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+ 
+ The Original Code is the "OGC Service Framework".
+ 
+ The Initial Developer of the Original Code is Spotimage S.A.
+ Portions created by the Initial Developer are Copyright (C) 2007
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s): 
+    Alexandre Robin <alexandre.robin@spotimage.fr>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.vast.ows.sos;
+
+import java.util.*;
+
+import org.vast.ows.OWSException;
+import org.vast.ows.OWSRequest;
+import org.vast.ows.util.Bbox;
+import org.vast.ows.util.TimeInfo;
+
+
+/**
+ * <p><b>Title:</b><br/>
+ * GetObservation Request
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ * Container for SOS GetObservation request parameters
+ * </p>
+ *
+ * <p>Copyright (c) 2007</p>
+ * @author Alexandre Robin
+ * @date Oct 09, 2007
+ * @version 1.0
+ */
+public class GetResultRequest extends OWSRequest
+{
+}
