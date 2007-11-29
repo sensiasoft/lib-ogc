a1a37cad31ebe3404a025549c4f582a1bd762b75
gberthiau <berthiau@nsstc.uah.edu>
2007-11-29 22:29:56 +0000

    git-svn-id: http://ows-service-framework.googlecode.com/svn/common@190 65eab6d0-0751-0410-986a-37c5601cf520

---
+++ src/org/vast/ows/sos/SOSCapabilitiesReader.java
@@ -0,0 +1,52 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1.1.html
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+ 
+ The Original Code is the "OGC Service Framework".
+ 
+ The Initial Developer of the Original Code is the VAST team at the University of Alabama in Huntsville (UAH). <http://vast.uah.edu> Portions created by the Initial Developer are Copyright (C) 2007 the Initial Developer. All Rights Reserved. Please Contact Mike Botts <mike.botts@uah.edu> for more information.
+ 
+ Contributor(s): 
+    Alexandre Robin <robin@nsstc.uah.edu>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.vast.ows.sps;
+
+import org.vast.ows.AbstractCapabilitiesReader;
+import org.vast.ows.OWSException;
+
+
+/**
+ * <p><b>Title:</b><br/>
+ * SOS Capabilities Reader
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ * Abstract SOS Capabilities Reader containing common code
+ * for all versions
+ * </p>
+ *
+ * <p>Copyright (c) 2006</p>
+ * @author Alexandre Robin
+ * @date Dec 20, 2006
+ * @version 1.0
+ */
+public abstract class SOSCapabilitiesReader extends AbstractCapabilitiesReader
+{
+    
+    @Override
+    protected String buildQuery() throws OWSException
+    {
+        String url = null;
+        url = this.server + "service=SOS&version=0.0.31&request=GetCapabilities";  
+        return url;
+    }
+}
