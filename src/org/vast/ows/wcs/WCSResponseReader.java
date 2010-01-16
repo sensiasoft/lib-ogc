8c16d4ff188b84e456f4467c081f6dd77da9478a
sensiasoft <alex.robin@sensiasoftware.com>
2010-01-16 22:04:52 +0000

    Updated WCS response reader to read result array as a block (this is slightly different from SOS response reader that reads array elements one by one in a streaming fashion)
    git-svn-id: http://ows-service-framework.googlecode.com/svn/trunk@326 65eab6d0-0751-0410-986a-37c5601cf520

--- src/org/vast/ows/wcs/WCSResponseReader.java
+++ src/org/vast/ows/wcs/WCSResponseReader.java
@@ -20,10 +20,130 @@
 
 package org.vast.ows.wcs;
 
-import org.vast.ogc.om.ObservationStreamReaderV10;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.vast.cdm.common.CDMException;
+import org.vast.cdm.common.DataHandler;
+import org.vast.data.DataArray;
+import org.vast.ogc.OGCException;
+import org.vast.ogc.OGCExceptionReader;
+import org.vast.sweCommon.SWECommonUtils;
+import org.vast.sweCommon.SWEFilter;
+import org.vast.sweCommon.SWEReader;
+import org.vast.sweCommon.URIStreamHandler;
+import org.vast.xml.DOMHelper;
+import org.vast.xml.DOMHelperException;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 
-public class WCSResponseReader extends ObservationStreamReaderV10
+/**
+ *  * <p><b>Title:</b>
+ * WCSResponseReader
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ * Read WCS response in SWE format. The actual format is plain O&M
+ * with a fixed size N-dimensional DataArray containing coverage data as the result.
+ * </p>
+ *
+ * <p>Copyright (c) 2008</p>
+ * @author Alexandre Robin
+ * @date Jan 16, 2010
+ * @version 1.0
+ */
+public class WCSResponseReader extends SWEReader
 {
-	
+    protected SWEFilter streamFilter;
+    protected DataArray coverageArray;
+    
+    
+    @Override
+    public void parse(InputStream inputStream, DataHandler handler) throws CDMException
+    {
+        try
+        {
+            dataHandler = handler;
+            streamFilter = new SWEFilter(inputStream);
+            streamFilter.setDataElementName("values");
+            
+            // parse xml header using DOMReader
+            DOMHelper dom = new DOMHelper(streamFilter, false);
+            OGCExceptionReader.checkException(dom);
+            
+            // find first Observation element
+            Element rootElement = dom.getRootElement();
+            NodeList elts = rootElement.getOwnerDocument().getElementsByTagNameNS("http://www.opengis.net/om/1.0", "Observation");
+            Element obsElt = (Element)elts.item(0); 
+            if (obsElt == null)
+                throw new CDMException("XML Response doesn't contain any Observation");
+            
+            // read result
+            SWECommonUtils utils = new SWECommonUtils();
+            Element resultArrayElt = dom.getElement(obsElt, "result/*");
+            
+            // read array
+            Element countElt = dom.getElement(resultArrayElt, "elementCount/Count");
+            String countValue = dom.getElementValue(countElt, "value");
+            int arraySize = Integer.parseInt(countValue);
+            coverageArray = new DataArray(arraySize);
+                        
+            // read array component structure and encoding
+            Element eltTypeElt = dom.getElement(resultArrayElt, "elementType");
+            Element encElt = dom.getElement(resultArrayElt, "encoding");
+            this.dataComponents = utils.readComponentProperty(dom, eltTypeElt);
+            this.dataEncoding = utils.readEncodingProperty(dom, encElt);
+            this.dataParser = createDataParser();
+            coverageArray.addComponent(this.dataComponents);
+            this.dataParser.setParentArray(coverageArray);
+            
+            // read external link if present
+            Element valElt = dom.getElement(resultArrayElt, "values");
+            valuesUri = valElt.getAttribute("xlink:href");
+            dataParser.parse(new BufferedInputStream(getDataStream()));
+        }
+        catch (IllegalStateException e)
+        {
+            throw new CDMException("No reader found for SWECommon", e);
+        }
+        catch (DOMHelperException e)
+        {
+            throw new CDMException("Error while parsing Observation XML", e);
+        }       
+        catch (OGCException e)
+        {
+            throw new CDMException(e.getMessage());
+        }
+    }
+    
+    
+    public DataArray getCoverageArray()
+    {
+        return coverageArray;
+    }
+    
+    
+    public InputStream getDataStream() throws CDMException
+    {
+        if (valuesUri != null && valuesUri.length()>0)
+        {
+            try
+            {
+                streamFilter.startReadingData();
+                streamFilter.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            
+            return URIStreamHandler.openStream(valuesUri);
+        }
+        else
+        {
+            streamFilter.startReadingData();
+            return streamFilter;
+        }
+    }
 }
