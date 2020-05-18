c374b523fa1247fa147ba3168f6845202fc80d62
Alex Robin <alex.robin@sensiasoft.com>
2020-05-18 14:53:44 +0200

    Removed HasUom from CountOrRange interface

--- swe-common-core/src/main/java/net/opengis/swe/v20/CountOrRange.java
+++ swe-common-core/src/main/java/net/opengis/swe/v20/CountOrRange.java
@@ -5,6 +5,6 @@ package net.opengis.swe.v20;
  * Tagging interface to allow processing of Count and CountRange components
  * with common logic
  */
-public interface CountOrRange extends SimpleComponent, HasUom, HasConstraints<AllowedValues> {
+public interface CountOrRange extends SimpleComponent, HasConstraints<AllowedValues> {
 
 }
