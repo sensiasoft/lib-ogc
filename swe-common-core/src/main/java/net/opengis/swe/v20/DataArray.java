37b2563de2c56aa44720b2216dedc09af6e84176
Alex Robin <alex.robin@sensiasoft.com>
2020-05-19 09:55:45 +0200

    Added comment to clarify how variable array size components are handled

--- swe-common-core/src/main/java/net/opengis/swe/v20/DataArray.java
+++ swe-common-core/src/main/java/net/opengis/swe/v20/DataArray.java
@@ -7,9 +7,9 @@ at http://mozilla.org/MPL/2.0/.
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 for the specific language governing rights and limitations under the License.
- 
+
 Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
- 
+
 ******************************* END LICENSE BLOCK ***************************/
 
 package net.opengis.swe.v20;
@@ -22,45 +22,56 @@ package net.opengis.swe.v20;
  */
 public interface DataArray extends BlockComponent
 {
-      
+
     @Override
     public DataArray copy();
-    
-    
+
+
+    /**
+     * Sets the elementCount property.<br/>
+     * If the component given as argument is already in the component tree, it
+     * will be used as a variable size component (in which case it must have
+     * an ID defined), otherwise the component will be used to set a fixed size
+     * for the array and must carry a non-negative value.
+     */
+    @Override
+    public void setElementCount(Count elementCount);
+
+
     /**
      * @return true if array has variable size
      */
     public boolean isVariableSize();
-    
-    
+
+
     /**
      * @return true if array has implicit variable size (i.e. the size is not
      * specified by any component but will be included implicitely in a stream
      * right before the array data).
      */
     public boolean isImplicitSize();
-    
-    
+
+
     /**
      * Gets the array size component whether the array has fixed, implicit
      * or variable size.
      * @return the Count component that contains the array size value
      */
     public Count getArraySizeComponent();
-    
-    
+
+
     /**
      * Updates the size of the array (and corresponding data block if set)
      * using the size value set in the size component
      */
     public void updateSize();
-    
-    
+
+
     /**
      * Updates the size of the array (and corresponding data block if set) to
      * the given size. If a size component is set, its value is also updated.
      * @param arraySize new array size
      */
     public void updateSize(int arraySize);
-    
+
 }
