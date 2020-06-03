966ebd60592ace9ac37f79fb08631c2784cf3dfe
Alex Robin <alex.robin@sensiasoft.com>
2020-06-03 07:14:48 +0200

    Added new tests for SWEHelper/SWEBuilders

--- swe-common-core/src/test/java/org/vast/swe/test/TestSweHelper.java
+++ swe-common-core/src/test/java/org/vast/swe/test/TestSweHelper.java
@@ -7,9 +7,9 @@ at http://mozilla.org/MPL/2.0/.
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 for the specific language governing rights and limitations under the License.
- 
+
 Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
- 
+
 ******************************* END LICENSE BLOCK ***************************/
 
 package org.vast.swe.test;
@@ -28,76 +28,157 @@ import org.vast.swe.SWEHelper;
 
 public class TestSweHelper
 {
-    SWEHelper fac = new SWEHelper();
+    SWEHelper helper = new SWEHelper();
     SWEUtils utils = new SWEUtils(SWEUtils.V2_0);
-    
-    
+
+
     @Test
     public void testCreateQuantities() throws Exception
     {
-        utils.writeComponent(System.out, fac.newQuantity(SWEHelper.getPropertyUri("AirTemperature"), "Air Temperature", "Temperature of air in the garden", "Cel"), false, true);
-        utils.writeComponent(System.out, fac.newQuantity(SWEHelper.getPropertyUri("Acceleration"), "Acceleration", null, "m/s2"), false, true);
+        utils.writeComponent(System.out, helper.createQuantity()
+            .definition(SWEHelper.getPropertyUri("AirTemperature"))
+            .label("Air Temperature")
+            .description("Temperature of air in the garden")
+            .uomCode("Cel")
+            .build(), false, true);
+
+        System.out.println();
+        System.out.println();
+
+        utils.writeComponent(System.out, helper.createQuantity()
+            .definition(SWEHelper.getPropertyUri("LinearAcceleration"))
+            .label("Acceleration")
+            .uomCode("m/s2")
+            .build(), false, true);
+
+        System.out.println();
+        System.out.println();
     }
-    
-    
-    @Test
+
+
+    /*@Test
     public void testCreateRasters() throws Exception
     {
         // RGB
         utils.writeComponent(System.out, fac.newRgbImage(640, 480, DataType.BYTE), false, true);
-    }
-    
-    
-    protected DataRecord createWeatherRecord() throws Exception
-    {
-        DataRecord rec = fac.newDataRecord(3);
-        rec.addField("time", fac.newTimeStampIsoUTC());
-        rec.addField("temp", fac.newQuantity(SWEHelper.getPropertyUri("AirTemperature"), "Air Temperature", null, "Cel"));
-        rec.addField("press", fac.newQuantity(SWEHelper.getPropertyUri("AtmosphericPressure"), "Air Pressure", null, "hPa"));
-        rec.addField("windSpeed", fac.newQuantity(SWEHelper.getPropertyUri("WindSpeed"), "Wind Speed", null, "km/h"));
-        return rec;
-    }
-    
-    
+    }*/
+
+
     @Test
     public void testCreateWeatherRecord() throws Exception
     {
-        utils.writeComponent(System.out, createWeatherRecord(), false, true);
+        utils.writeComponent(System.out, helper.createDataRecord()
+            .label("Weather Record")
+            .addSamplingTimeIsoUTC("time")
+            .addField("temp", helper.createQuantity()
+                .definition(SWEHelper.getPropertyUri("AirTemperature"))
+                .label("Air Temperature")
+                .uomCode("Cel")
+                .build())
+            .addField("press", helper.createQuantity()
+                .definition(SWEHelper.getPropertyUri("AtmosphericPressure"))
+                .label("Air Pressure")
+                .uomCode("hPa")
+                .build())
+            .addField("windSpeed", helper.createQuantity()
+                .definition(SWEHelper.getPropertyUri("WindSpeed"))
+                .label("Wind Speed")
+                .uomCode("km/h")
+                .build())
+            .build(), false, true);
+
+        System.out.println();
+        System.out.println();
     }
-    
-    
+
+
     @Test
-    public void testWrapWithTimeStamp() throws Exception
+    public void testCreateMixedTypeRecord() throws Exception
     {
-        DataRecord rec = createWeatherRecord();
-        rec.setName("weather");
-        DataArray img = fac.newRgbImage(800, 600, DataType.FLOAT);
-        img.setName("img");
-        DataRecord all = fac.wrapWithTimeStamp(fac.newTimeStampIsoUTC(), rec, img);
-        utils.writeComponent(System.out, all, false, true);
+        utils.writeComponent(System.out, helper.createDataRecord()
+            .label("Mixed Type Record")
+            .addSamplingTimeIsoUTC("time")
+            .addField("boolean", helper.createBoolean()
+                .definition(SWEHelper.getPropertyUri("AboveThreshold"))
+                .build())
+            .addField("cat", helper.createCategory()
+                .definition(SWEHelper.getPropertyUri("Species"))
+                .label("Species Name")
+                .build())
+            .addField("text", helper.createText()
+                .definition(SWEHelper.getPropertyUri("VIN"))
+                .label("Vehicle Identification Number")
+                .visitor(t -> System.out.println(t + "@" + System.identityHashCode(t)))
+                .build())
+            .build(), false, true);
+
+        System.out.println();
+        System.out.println();
     }
-    
-    
+
+
+    @Test
+    public void testNestedRecords() throws Exception
+    {
+        utils.writeComponent(System.out, helper.createDataRecord()
+            .label("Parent Record")
+            .addSamplingTimeIsoUTC("time")
+            .addField("boolean", helper.createBoolean()
+                .definition(SWEHelper.getPropertyUri("Flag"))
+                .build())
+            .addField("child", helper.createRecord()
+                .definition(SWEHelper.getPropertyUri("VIN"))
+                .label("Child Record")
+                .addField("scan_start", helper.createTime()
+                    .uomCode("ms")
+                    .value(10245)
+                    .build())
+                .addField("num_samples", helper.createCount()
+                    .value(2400)
+                    .build())
+                .build())
+            .build(), false, true);
+
+        System.out.println();
+        System.out.println();
+    }
+
+
     @Test
     public void testCreateDefaultEncodingForImage() throws Exception
     {
-        DataArray array = fac.newRgbImage(640, 480, DataType.BYTE);
+        DataArray array = helper.newRgbImage(640, 480, DataType.BYTE);
         DataEncoding encoding = SWEHelper.getDefaultEncoding(array);
         assertEquals(BinaryEncodingImpl.class, encoding.getClass());
         utils.writeEncoding(System.out, encoding, true);
     }
-    
-    
+
+
     @Test
     public void testGetComponentByPath() throws Exception
     {
-        DataRecord rec = createWeatherRecord();
+        DataRecord rec = helper.createRecord()
+            .addSamplingTimeIsoUTC("time")
+            .addField("temp", helper.createQuantity().build())
+            .addField("press", helper.createQuantity().build())
+            .addField("rec", helper.createRecord()
+                .addField("flag1", helper.createBoolean().build())
+                .addField("status", helper.createCategory().build())
+                .build())
+            .build();
+
         DataComponent c;
-        
+
         c = SWEHelper.findComponentByPath(rec, "temp");
         assertEquals(c.getName(), "temp");
-        
+
         c = SWEHelper.findComponentByPath(rec, "press");
         assertEquals(c.getName(), "press");
+
+        c = SWEHelper.findComponentByPath(rec, "rec/flag1");
+        assertEquals(c.getName(), "flag1");
+
+        c = SWEHelper.findComponentByPath(rec, "rec/status");
+        assertEquals(c.getName(), "status");
     }
 }
