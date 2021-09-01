ca5d5a5511eb7093d19db6788b440024ad216416
Alex Robin <alex.robin@sensiasoft.com>
2021-09-01 22:52:10 +0200

    [OM] Added moving feature to use as generic FOI when location/orientation is provided in separate datastream

---
+++ swe-common-om/src/main/java/org/vast/ogc/om/MovingFeature.java
@@ -0,0 +1,34 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+The contents of this file are subject to the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the License.
+ 
+Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.vast.ogc.om;
+
+import javax.xml.namespace.QName;
+import org.vast.ogc.gml.GenericFeatureImpl;
+
+
+public class MovingFeature extends GenericFeatureImpl
+{
+    private static final long serialVersionUID = 3975647873045316629L;
+    public static final String MF_NS_PREFIX = "mf";
+    public static final String MF_NS_URI = "http://www.opengis.net/movingfeatures/1.0";
+    
+    public static final QName MOVING_FEATURE = new QName(MF_NS_URI, "MovingFeature", MF_NS_PREFIX);
+    
+    
+    public MovingFeature()
+    {
+        super(MOVING_FEATURE);
+    }
+}
