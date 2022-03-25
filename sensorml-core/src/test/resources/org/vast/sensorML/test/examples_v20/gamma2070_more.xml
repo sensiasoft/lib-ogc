sensiasoft/lib-ogc

commit 7cd0b4bed8761e7da82d3be41a08ca7e56dd0d91

[SML] Removed intermediate ISO datatypes in JSON serialization

sensorml-core/src/main/java/org/vast/sensorML/json/SMLJsonStreamReader.java
@@ -16,6 +16,8 @@ package org.vast.sensorML.json;
 
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.HashSet;
+import org.vast.json.JsonStreamException;
 import org.vast.sensorML.SMLStaxBindings;
 import org.vast.swe.json.SWEJsonStreamReader;
 import com.google.gson.stream.JsonReader;
@@ -23,16 +25,26 @@ import com.google.gson.stream.JsonReader;
 
 public class SMLJsonStreamReader extends SWEJsonStreamReader
 {
-        
+    protected HashSet<String> isoStringNames;
+    
+    
+    protected static class JsonSmlContext extends JsonContext
+    {
+        public boolean isIsoObject;
+    }
+    
+    
     public SMLJsonStreamReader(InputStream is, Charset charset)
     {
         super(is, charset);
+        currentContext = new JsonSmlContext();
     }
     
     
     public SMLJsonStreamReader(JsonReader reader)
     {
         super(reader);
+        currentContext = new JsonSmlContext();
     }
     
     
@@ -49,6 +61,16 @@ public class SMLJsonStreamReader extends SWEJsonStreamReader
         // XML inline values
         addSpecialNames(inlineValueNames,
             "axis", "setValue", "setStatus", "setMode", "CharacterString", "URL");
+        
+        // ISO data types
+        isoStringNames = new HashSet<String>();
+        addSpecialNames(isoStringNames,
+            "individualName", "organisationName", "positionName",
+            "hoursOfService", "contactInstructions",
+            "voice", "facsimile",
+            "deliveryPoint", "city", "administrativeArea", "postalCode", "country", "electronicMailAddress",
+            "linkage", "protocol", "applicationProfile", "name", "description", 
+            "useLimitation");
     }
 
 
@@ -73,6 +95,65 @@ public class SMLJsonStreamReader extends SWEJsonStreamReader
             return "capability";
         else
             return super.getSingularName(name);
+    }
+    
+    
+    protected void pushContext(String eltName)
+    {
+        JsonContext prevContext = currentContext;
+        currentContext = new JsonSmlContext();
+        currentContext.parent = prevContext;
+        currentContext.eltName = eltName;
+        ((JsonSmlContext)currentContext).isIsoObject = ((JsonSmlContext)prevContext).isIsoObject;
+    }
+    
+    
+    @Override
+    public int next() throws JsonStreamException
+    {
+        if (((JsonSmlContext)currentContext).isIsoObject && isoStringNames.contains(currentContext.eltName) && currentContext.consumeText)
+            return nextTag();
+        else
+            return super.next();
+    }
+    
+    
+    @Override
+    public int nextTag() throws JsonStreamException
+    {
+        if (currentContext.eltName != null)
+        {
+            String parentName = currentContext.eltName;
+            if (parentName.startsWith("CI_") || parentName.startsWith("MD_"))
+                ((JsonSmlContext)currentContext).isIsoObject = true;
+        }
+        
+        if (((JsonSmlContext)currentContext).isIsoObject && isoStringNames.contains(currentContext.eltName) && currentContext.consumeText)
+        {
+            currentContext.consumeText = false;
+            pushContext("CharacterString");
+            currentContext.text = currentContext.parent.text;
+            currentContext.isTextOnly = true;
+            currentContext.consumeText = true;
+            return eventType = START_ELEMENT;
+        }
+        else
+            return super.nextTag();
+    }
+    
+    
+    @Override
+    protected boolean isXmlAttribute(String name)
+    {
+        if ("name".equals(name) && currentContext.eltName != null)
+        {
+            String parentName = currentContext.eltName;
+            if (parentName.startsWith("CI_") || parentName.startsWith("MD_"))
+                return false;
+        }
+
+        return super.isXmlAttribute(name);
+            
     }
     
 }

sensorml-core/src/main/java/org/vast/sensorML/json/SMLJsonStreamWriter.java
@@ -16,15 +16,24 @@ package org.vast.sensorML.json;
 
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.util.Set;
 import org.vast.json.JsonStreamException;
 import org.vast.ogc.gml.GMLStaxBindings;
 import org.vast.sensorML.SMLStaxBindings;
 import org.vast.swe.json.SWEJsonStreamWriter;
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.stream.JsonWriter;
 
 
 public class SMLJsonStreamWriter extends SWEJsonStreamWriter
 {
+    static Set<String> ISO_BASIC_TYPES = ImmutableSet.of(
+        "Boolean", "CharacterString", "Date", "Decimal", "Real", "Integer"
+    );
+    
+    static Set<String> ISO_CODELIST_TYPES = ImmutableSet.of(
+        "CI_RoleCode"
+    );
         
     public SMLJsonStreamWriter(OutputStream os, Charset charset)
     {
@@ -73,9 +82,39 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
     @Override
     public void writeAttribute(String localName, String value) throws JsonStreamException
     {
-        if (localName.equals("codeSpace"))
+        if ("codeSpace".equals(localName)) // to skip UID codespace
+            return;
+        else if (ISO_BASIC_TYPES.contains(currentContext.eltName)) // skip all basic types attributes
             return;
         else
             super.writeAttribute(localName, value);
+    }
+    
+    
+    @Override
+    public void writeStartElement(String prefix, String localName, String namespaceURI) throws JsonStreamException
+    {
+        // keep only ISO basic types value
+        if (ISO_BASIC_TYPES.contains(localName))
+        {
+            pushContext(localName);
+            currentContext.skipParent = true;
+        }
+        else
+            super.writeStartElement(prefix, localName, namespaceURI);
+    }
+    
+    
+    @Override
+    public void writeEndElement() throws JsonStreamException
+    {
+        // keep only ISO basic types value
+        if (ISO_BASIC_TYPES.contains(currentContext.eltName))
+        {
+            super.writeEndElement();
+            currentContext.skipParent = true;
+        }
+        else
+            super.writeEndElement();
     }
 }

sensorml-core/src/test/resources/org/vast/sensorML/test/examples_v20/gamma2070_more.xml
@@ -256,9 +256,7 @@ xsi:schemaLocation="http://www.opengis.net/sensorML/2.0 http://schemas.opengis.n
 							</gmd:address>
 						</gmd:CI_Contact>
 					</gmd:contactInfo>
-                    <gmd:role>
-                        <gmd:CI_RoleCode codeList="companyRoles" codeListValue="manufacturer" />
-                    </gmd:role>
+          <gmd:role gco:nilReason="inapplicable"/>
 				</gmd:CI_ResponsibleParty>
 			</sml:contact>
 		</sml:ContactList>

sensorml-core/src/test/resources/org/vast/sensorML/test/examples_v20/Davis_7817_complete.xml
@@ -173,7 +173,7 @@
                   </gmd:CI_Contact>
                </gmd:contactInfo>
                <gmd:role>
-                  <gmd:CI_RoleCode codeList="companyRoles" codeListValue="manufacturer" />
+                  <gmd:CI_RoleCode codeList="http://sensorml.com/def/roles" codeListValue="Manufacturer" />
                </gmd:role>
             </gmd:CI_ResponsibleParty>
          </sml:contact>

commit 79f150b97f215cb863d69fe77d048f43cc8be339

[SWE] Fixed regression serializing Boolean objects introduced in 7cd0b4b + added unit test

sensorml-core/src/main/java/org/vast/sensorML/json/SMLJsonStreamReader.java
@@ -25,46 +25,40 @@ import com.google.gson.stream.JsonReader;
 
 public class SMLJsonStreamReader extends SWEJsonStreamReader
 {
-    protected HashSet<String> isoStringNames;
+    protected static final HashSet<String> ISO_STRING_NAMES = new HashSet<String>();
+    protected JsonContext isoEltRootCtx;
     
-    
-    protected static class JsonSmlContext extends JsonContext
+    static
     {
-        public boolean isIsoObject;
+        initSpecialNames();
     }
     
     
     public SMLJsonStreamReader(InputStream is, Charset charset)
     {
         super(is, charset);
-        currentContext = new JsonSmlContext();
     }
     
     
     public SMLJsonStreamReader(JsonReader reader)
     {
         super(reader);
-        currentContext = new JsonSmlContext();
     }
     
     
-    @Override
-    protected void initSpecialNames()
+    protected static void initSpecialNames()
     {
-        super.initSpecialNames();
-        
         // XML attributes
-        addSpecialNames(xmlAttNames,
+        addSpecialNames(XML_ATT_NAMES,
             "srsName", "srsDimension", "indeterminatePosition",
             "ref", "codeList", "codeListValue", "nilReason");
         
         // XML inline values
-        addSpecialNames(inlineValueNames,
+        addSpecialNames(INLINE_VALUES_NAMES,
             "axis", "setValue", "setStatus", "setMode", "CharacterString", "URL");
         
         // ISO data types
-        isoStringNames = new HashSet<String>();
-        addSpecialNames(isoStringNames,
+        addSpecialNames(ISO_STRING_NAMES,
             "individualName", "organisationName", "positionName",
             "hoursOfService", "contactInstructions",
             "voice", "facsimile",
@@ -98,20 +92,33 @@ public class SMLJsonStreamReader extends SWEJsonStreamReader
     }
     
     
+    @Override
     protected void pushContext(String eltName)
     {
-        JsonContext prevContext = currentContext;
-        currentContext = new JsonSmlContext();
-        currentContext.parent = prevContext;
-        currentContext.eltName = eltName;
-        ((JsonSmlContext)currentContext).isIsoObject = ((JsonSmlContext)prevContext).isIsoObject;
+        super.pushContext(eltName);
+        
+        // mark begin of root ISO object
+        if (eltName != null && isoEltRootCtx == null &&
+           (eltName.startsWith("CI_") || eltName.startsWith("MD_")))
+            isoEltRootCtx = currentContext;
+    }
+    
+    
+    @Override
+    protected void popContext()
+    {
+        // mark end of ISO object
+        if (currentContext == isoEltRootCtx)
+            isoEltRootCtx = null;
+        
+        super.popContext();
     }
     
     
     @Override
     public int next() throws JsonStreamException
     {
-        if (((JsonSmlContext)currentContext).isIsoObject && isoStringNames.contains(currentContext.eltName) && currentContext.consumeText)
+        if (isoEltRootCtx != null && ISO_STRING_NAMES.contains(currentContext.eltName) && currentContext.consumeText)
             return nextTag();
         else
             return super.next();
@@ -121,14 +128,7 @@ public class SMLJsonStreamReader extends SWEJsonStreamReader
     @Override
     public int nextTag() throws JsonStreamException
     {
-        if (currentContext.eltName != null)
-        {
-            String parentName = currentContext.eltName;
-            if (parentName.startsWith("CI_") || parentName.startsWith("MD_"))
-                ((JsonSmlContext)currentContext).isIsoObject = true;
-        }
-        
-        if (((JsonSmlContext)currentContext).isIsoObject && isoStringNames.contains(currentContext.eltName) && currentContext.consumeText)
+        if (isoEltRootCtx != null && ISO_STRING_NAMES.contains(currentContext.eltName) && currentContext.consumeText)
         {
             currentContext.consumeText = false;
             pushContext("CharacterString");
@@ -145,15 +145,10 @@ public class SMLJsonStreamReader extends SWEJsonStreamReader
     @Override
     protected boolean isXmlAttribute(String name)
     {
-        if ("name".equals(name) && currentContext.eltName != null)
-        {
-            String parentName = currentContext.eltName;
-            if (parentName.startsWith("CI_") || parentName.startsWith("MD_"))
+        if (isoEltRootCtx != null && "name".equals(name))
                 return false;
-        }
-
+        
         return super.isXmlAttribute(name);
-            
     }
     
 }

sensorml-core/src/main/java/org/vast/sensorML/json/SMLJsonStreamWriter.java
@@ -34,7 +34,17 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
     static Set<String> ISO_CODELIST_TYPES = ImmutableSet.of(
         "CI_RoleCode"
     );
-        
+    
+    
+    static
+    {
+        initSpecialNames();
+    }
+    
+
+    protected JsonContext isoEltRootCtx;
+    
+    
     public SMLJsonStreamWriter(OutputStream os, Charset charset)
     {
         super(os, charset);
@@ -47,12 +57,9 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
     }
     
     
-    @Override
-    protected void initSpecialNames()
+    protected static void initSpecialNames()
     {
-        super.initSpecialNames();
-        
-        addSpecialNames(arrays, SMLStaxBindings.NS_URI,
+        addSpecialNames(ARRAYS, SMLStaxBindings.NS_URI,
             "keywords", "identification", "classification",
             "validTime", "securityConstraints", "legalConstraints",
             "characteristics", "capabilities", "contacts",
@@ -65,7 +72,7 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
             "setValue", "setArrayValues", "setConstraint",
             "setMode", "setStatus");
 
-        addSpecialNames(arrays, GMLStaxBindings.NS_URI, "name");
+        addSpecialNames(ARRAYS, GMLStaxBindings.NS_URI, "name");
     }
     
     
@@ -80,11 +87,34 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
     
     
     @Override
+    protected void pushContext(String eltName)
+    {
+        super.pushContext(eltName);
+        
+        // mark begin of root ISO object
+        if (eltName != null && isoEltRootCtx == null &&
+           (eltName.startsWith("CI_") || eltName.startsWith("MD_")))
+            isoEltRootCtx = currentContext;
+    }
+    
+    
+    @Override
+    protected void popContext()
+    {
+        // mark end of ISO object
+        if (currentContext == isoEltRootCtx)
+            isoEltRootCtx = null;
+        
+        super.popContext();
+    }
+    
+    
+    @Override
     public void writeAttribute(String localName, String value) throws JsonStreamException
     {
         if ("codeSpace".equals(localName)) // to skip UID codespace
             return;
-        else if (ISO_BASIC_TYPES.contains(currentContext.eltName)) // skip all basic types attributes
+        else if (isoEltRootCtx != null && ISO_BASIC_TYPES.contains(currentContext.eltName)) // skip all basic types attributes
             return;
         else
             super.writeAttribute(localName, value);
@@ -95,7 +125,7 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
     public void writeStartElement(String prefix, String localName, String namespaceURI) throws JsonStreamException
     {
         // keep only ISO basic types value
-        if (ISO_BASIC_TYPES.contains(localName))
+        if (isoEltRootCtx != null && ISO_BASIC_TYPES.contains(localName))
         {
             pushContext(localName);
             currentContext.skipParent = true;
@@ -109,7 +139,7 @@ public class SMLJsonStreamWriter extends SWEJsonStreamWriter
     public void writeEndElement() throws JsonStreamException
     {
         // keep only ISO basic types value
-        if (ISO_BASIC_TYPES.contains(currentContext.eltName))
+        if (isoEltRootCtx != null && ISO_BASIC_TYPES.contains(currentContext.eltName))
         {
             super.writeEndElement();
             currentContext.skipParent = true;

swe-common-core/src/main/java/org/vast/swe/json/SWEJsonStreamReader.java
@@ -26,31 +26,33 @@ import com.google.gson.stream.JsonReader;
 
 public class SWEJsonStreamReader extends JsonStreamReader
 {
-    protected final static String NO_PARENT = "";
+    protected static final String NO_PARENT = "";
+    protected static final HashSet<String> XML_ATT_NAMES = new HashSet<String>();
+    protected static final HashSet<String> INLINE_VALUES_NAMES = new HashSet<String>();
+    protected static final Map<String, Map<String, String>> VALUE_ARRAYS = new HashMap<String, Map<String, String>>();
     
-    protected HashSet<String> xmlAttNames = new HashSet<String>();
-    protected HashSet<String> inlineValueNames = new HashSet<String>();
-    protected Map<String, Map<String, String>> valueArrays = new HashMap<String, Map<String, String>>();
+    static
+    {
+        initSpecialNames();
+    }
     
     
     public SWEJsonStreamReader(InputStream is, Charset charset)
     {
         super(is, charset);
-        initSpecialNames();
     }
     
     
     public SWEJsonStreamReader(JsonReader reader)
     {
         super(reader);
-        initSpecialNames();        
     }
     
     
-    protected void initSpecialNames()
+    protected static void initSpecialNames()
     {
         // XML attributes
-        addSpecialNames(xmlAttNames,
+        addSpecialNames(XML_ATT_NAMES,
                 "name", "href", "title", "role", "arcrole", "code", "reason", "id", "definition",
                 "referenceFrame", "localFrame", "referenceTime", "axisID", "updatable", "optional",
                 "collapseWhiteSpaces", "decimalSeparator", "tokenSeparator", "blockSeparator",
@@ -58,25 +60,25 @@ public class SWEJsonStreamReader extends JsonStreamReader
                 "compression", "encryption", "paddingBytes-after", "paddingBytes-before", "byteLength");
         
         // XML inline values
-        addSpecialNames(inlineValueNames, "nilValue");
+        addSpecialNames(INLINE_VALUES_NAMES, "nilValue");
         
         // value arrays
-        addSpecialNamesWithParent(valueArrays, "CountRange", "value");
-        addSpecialNamesWithParent(valueArrays, "QuantityRange", "value");
-        addSpecialNamesWithParent(valueArrays, "CategoryRange", "value");
-        addSpecialNamesWithParent(valueArrays, "TimeRange", "value");
-        addSpecialNamesWithParent(valueArrays, "interval", "interval");
+        addSpecialNamesWithParent(VALUE_ARRAYS, "CountRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, "QuantityRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, "CategoryRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, "TimeRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, "interval", "interval");
     }
     
     
-    protected void addSpecialNames(HashSet<String> nameList, String... names)
+    protected static void addSpecialNames(HashSet<String> nameList, String... names)
     {
         for (String name: names)
             nameList.add(name);
     }
     
     
-    protected void addSpecialNamesWithParent(Map<String, Map<String, String>> nameMaps, String parentName, String... names)
+    protected static void addSpecialNamesWithParent(Map<String, Map<String, String>> nameMaps, String parentName, String... names)
     {
         Map<String, String> nameMapForParent = nameMaps.get(parentName);
         if (nameMapForParent == null)
@@ -93,14 +95,14 @@ public class SWEJsonStreamReader extends JsonStreamReader
     @Override
     protected boolean isXmlAttribute(String name)
     {
-        return ( (name.charAt(0) == ATT_PREFIX) || xmlAttNames.contains(name) );
+        return ( (name.charAt(0) == ATT_PREFIX) || XML_ATT_NAMES.contains(name) );
     }
     
 
     @Override
     protected boolean isInlineValue(String name)
     {
-        return inlineValueNames.contains(name);
+        return INLINE_VALUES_NAMES.contains(name);
     }
     
     
@@ -130,7 +132,7 @@ public class SWEJsonStreamReader extends JsonStreamReader
         if (currentContext.parent != null && currentContext.parent.eltName != null)
             parentName = currentContext.parent.eltName;
         
-        return isSpecialPath(valueArrays, parentName, name);
+        return isSpecialPath(VALUE_ARRAYS, parentName, name);
     }
 
 

swe-common-core/src/main/java/org/vast/swe/json/SWEJsonStreamWriter.java
@@ -24,13 +24,19 @@ import com.google.gson.stream.JsonWriter;
 
 public class SWEJsonStreamWriter extends JsonStreamWriter
 {
-    protected final static String NO_PARENT = "";
-    protected final static String NO_NS = "";
+    protected static final String NO_PARENT = "";
+    protected static final String NO_NS = "";
     
     // array name mappings (key is parentName, subkey is localName, value is NS uri)
-    protected Map<String, Map<String, String>> arrays = new HashMap<String, Map<String, String>>();
-    protected Map<String, Map<String, String>> numerics = new HashMap<String, Map<String, String>>();
-    protected Map<String, Map<String, String>> valueArrays = new HashMap<String, Map<String, String>>();
+    protected static final Map<String, Map<String, String>> ARRAYS = new HashMap<String, Map<String, String>>();
+    protected static final Map<String, Map<String, String>> NUMERICS = new HashMap<String, Map<String, String>>();
+    protected static final Map<String, Map<String, String>> VALUE_ARRAYS = new HashMap<String, Map<String, String>>();
+    
+    
+    static
+    {
+        initSpecialNames();
+    }
     
     
     public SWEJsonStreamWriter(OutputStream os, Charset charset)
@@ -49,39 +55,39 @@ public class SWEJsonStreamWriter extends JsonStreamWriter
     }
     
     
-    protected void initSpecialNames()
+    protected static void initSpecialNames()
     {
         // elements to encode as JSON arrays
-        addSpecialNames(arrays, NO_NS, "field", "coordinate", "item", "quality", "member");
-        addSpecialNamesWithParent(arrays, NO_NS, "AllowedValues", "value", "interval");
-        addSpecialNamesWithParent(arrays, NO_NS, "AllowedTimes", "value", "interval");
-        addSpecialNamesWithParent(arrays, NO_NS, "AllowedTokens", "value");
-        addSpecialNamesWithParent(valueArrays, NO_NS, "CountRange", "value");
-        addSpecialNamesWithParent(valueArrays, NO_NS, "QuantityRange", "value");
-        addSpecialNamesWithParent(valueArrays, NO_NS, "CategoryRange", "value");
-        addSpecialNamesWithParent(valueArrays, NO_NS, "TimeRange", "value");
-        addSpecialNamesWithParent(valueArrays, NO_NS, "interval", "interval");
+        addSpecialNames(ARRAYS, NO_NS, "field", "coordinate", "item", "quality", "member");
+        addSpecialNamesWithParent(ARRAYS, NO_NS, "AllowedValues", "value", "interval");
+        addSpecialNamesWithParent(ARRAYS, NO_NS, "AllowedTimes", "value", "interval");
+        addSpecialNamesWithParent(ARRAYS, NO_NS, "AllowedTokens", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, NO_NS, "CountRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, NO_NS, "QuantityRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, NO_NS, "CategoryRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, NO_NS, "TimeRange", "value");
+        addSpecialNamesWithParent(VALUE_ARRAYS, NO_NS, "interval", "interval");
         
         // elements with numerical values
-        addSpecialNames(numerics, NO_NS, "nilValue", "paddingBytes-after", "paddingBytes-before", "byteLength", "significantBits", "bitLength");
-        addSpecialNamesWithParent(numerics, NO_NS, "Count", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "Quantity", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "Time", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "CountRange", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "QuantityRange", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "AllowedValues", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "value", "value");
-        addSpecialNamesWithParent(numerics, NO_NS, "interval", "interval");
+        addSpecialNames(NUMERICS, NO_NS, "nilValue", "paddingBytes-after", "paddingBytes-before", "byteLength", "significantBits", "bitLength");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "Count", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "Quantity", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "Time", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "CountRange", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "QuantityRange", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "AllowedValues", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "value", "value");
+        addSpecialNamesWithParent(NUMERICS, NO_NS, "interval", "interval");
     }
     
     
-    protected void addSpecialNames(Map<String, Map<String, String>> nameMaps, String namespaceURI, String... names)
+    protected static void addSpecialNames(Map<String, Map<String, String>> nameMaps, String namespaceURI, String... names)
     {
         addSpecialNamesWithParent(nameMaps, namespaceURI, NO_PARENT, names);
     }
     
     
-    protected void addSpecialNamesWithParent(Map<String, Map<String, String>> nameMaps, String namespaceURI, String parentName, String... names)
+    protected static void addSpecialNamesWithParent(Map<String, Map<String, String>> nameMaps, String namespaceURI, String parentName, String... names)
     {
         Map<String, String> nameMapForParent = nameMaps.get(parentName);
         if (nameMapForParent == null)
@@ -129,7 +135,7 @@ public class SWEJsonStreamWriter extends JsonStreamWriter
         if (currentContext != null && currentContext.eltName != null)
             parentName = currentContext.eltName;
             
-        return isSpecialPath(arrays, parentName, namespaceURI, localName);
+        return isSpecialPath(ARRAYS, parentName, namespaceURI, localName);
     }
     
     
@@ -150,7 +156,7 @@ public class SWEJsonStreamWriter extends JsonStreamWriter
         if (currentContext.parent != null && currentContext.parent.eltName != null)
             parentName = currentContext.parent.eltName;
         
-        return isSpecialPath(valueArrays, parentName, namespaceURI, localName);
+        return isSpecialPath(VALUE_ARRAYS, parentName, namespaceURI, localName);
     }
     
     
@@ -160,7 +166,7 @@ public class SWEJsonStreamWriter extends JsonStreamWriter
         if (!super.isNumericValue(value))
             return false;
         
-        return isSpecialPath(numerics, currentContext.parent.eltName, null, currentContext.eltName);
+        return isSpecialPath(NUMERICS, currentContext.parent.eltName, null, currentContext.eltName);
     }
 
 

sensorml-core/src/test/resources/org/vast/sensorML/test/examples_v20/gamma2070_more.xml
@@ -150,6 +150,11 @@ xsi:schemaLocation="http://www.opengis.net/sensorML/2.0 http://schemas.opengis.n
 									<swe:value>Aluminum</swe:value>
 								</swe:Category>
 							</swe:field>
+							<swe:field name="Hardened">
+								<swe:Boolean definition="http://sensorml.com/ont/swe/property/Hardened">
+									<swe:value>true</swe:value>
+								</swe:Boolean>
+							</swe:field>
 						</swe:DataRecord>
 					</swe:field>
 				</swe:DataRecord>